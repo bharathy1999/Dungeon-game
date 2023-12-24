7dca0a3463ed6fbd78f8fe72584b403e7ca036c6
bharathy1999 <121527343+bharathy1999@users.noreply.github.com>
2023-12-24 16:29:00 +0530

    Update Dungeongame.java

--- Dungeongame.java
+++ Dungeongame.java
@@ -1,87 +1,134 @@
 package com.bharathy.show;
+
+public class Players {  
+        int row;
+        int col;
+        Players(int row,int col){
+        	this.row=row;
+        	this.col=col;
+        }
+}
+
+
+
+package com.bharathy.show;
 import java.util.Scanner;
 public class Dungeongame {
-
+         static int stepcalculate(Players start,Players target) {
+        	  int steps=0;
+        	  
+              
+              int i=0;   
+              if(start.row>target.row) {
+           	   steps+=start.row-target.row;        	
+              }
+              
+              if(start.row<target.row)  {
+           	   steps+=target.row-start.row;
+              }
+              
+              if(start.col>target.col) {
+           	   steps+=start.col-target.col;
+              }
+              
+              if(start.col<target.col) {
+           	   steps+=target.col-start.col;
+              } 
+              
+              return steps;
+              
+          }
+         static String footprint(int steps,Players adventure,Players gold,Players monster ) {
+        	 
+           	  StringBuilder strb=new StringBuilder();
+                 strb.append("("+(adventure.row+","+adventure.col)+")-->");
+                 int i=0;
+                while(i<steps) {
+                	if(monster!=null&&adventure.row<gold.row&&adventure.col!=monster.col) {
+                		adventure.row++;
+                	}
+                	else if(monster!=null&&adventure.row>gold.row&&adventure.col!=monster.col) {
+                		adventure.row--;
+                	}
+                	else if(monster!=null&&adventure.col>gold.col&&adventure.row!=monster.row) {
+                		adventure.col--;
+                	}
+                	else if(monster!=null&&adventure.col<gold.col&&adventure.row!=monster.row) {
+                		adventure.col++;
+                	}
+                	
+                	else if(adventure.row==gold.row) {
+//                		adventure.col=adventure.col>gold.col?adventure.col--:adventure.col++;
+                		if(adventure.col<gold.col) {
+                			adventure.col++;
+                		}
+                		if(adventure.col>gold.col) {
+                			adventure.col--;
+                		}
+                	}
+                	else if(adventure.col==gold.col) {
+//                		adventure.row=adventure.row>gold.row?adventure.row--:adventure.row++;
+                		if(adventure.row>gold.row) {
+                			adventure.row--;
+                		}
+                		if(adventure.row<gold.row) {
+                			adventure.row++;
+                		}
+                	}
+                	strb.append("("+adventure.row+","+adventure.col+")-->");
+                	
+                	i++;
+                }
+                strb.delete(strb.length()-3, strb.length());
+                 return strb.toString();
+             
+         }
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the dimensions of dungeon :");
         int row=scanner.nextInt();
         int column=scanner.nextInt();
         char dungeon [][]=new char[row][column];
+        
         System.out.println("Enter the adventure position :");
         int adventureinrow=scanner.nextInt();
         int adventureincolumn=scanner.nextInt();
-        dungeon[adventureinrow-1][adventureincolumn-1]='A';
-        System.out.println("Enter a monster position :");
+        Players adventure=new Players(adventureinrow,adventureincolumn);
+        
+        System.out.println("Enter the monster position :");
         int monsterinrow=scanner.nextInt();
         int monsterincolumn=scanner.nextInt();
-        dungeon[monsterinrow][monsterincolumn]='M';
+        Players monster=new Players(monsterinrow,monsterincolumn);
+        
+        System.out.println("Enter the trigger position :");
+        int triggerinrow=scanner.nextInt();
+        int triggerincolumn=scanner.nextInt();
+        Players trigger=new Players(triggerinrow,triggerincolumn);
+        
         System.out.println("Enter the gold position :");
         int goldinrow=scanner.nextInt();
         int goldincolumn=scanner.nextInt();
-        dungeon[goldinrow-1][goldincolumn-1]='G';
+        Players gold=new Players(goldinrow,goldincolumn);
         
-        int steps=0;
-        StringBuilder strb=new StringBuilder();
-        strb.append("("+(adventureinrow+","+adventureincolumn)+")");
-        int i=0;
-        if(adventureinrow>goldinrow) {
-        	steps+=adventureinrow-goldinrow;
 
-        	while(i<steps) {
-        		i++;
-        		strb.append("-->("+((adventureinrow-i)+","+adventureincolumn)+")");
-        	}
-        }
-        
-        else {
-        	steps+=goldinrow-adventureinrow;
-        	while(i<steps) {
-    		i++;
-    		strb.append("-->("+((adventureinrow+i)+","+adventureincolumn)+")") ;
-    	}
-        }
-        int j=0;
-        
-        if(adventureincolumn>goldincolumn) {
-        	steps+=adventureincolumn-goldincolumn;
-        	while(j<(adventureincolumn-goldincolumn)) {
-        		j++;
-        		strb.append("-->("+(goldinrow+","+(adventureincolumn-j))+")");
-        	}
-        }
-        
-        else {
-        	steps+=goldincolumn-adventureincolumn;
-        	while(j<(goldincolumn-adventureincolumn)) {
-        		j++;
-        		strb.append("-->("+(goldinrow+","+(adventureincolumn+j))+")");
-        	}
-        }
-        
-       
-       int monsteps=0;
-       if(monsterinrow>goldinrow) {
-    	   monsteps+=monsterinrow-goldinrow;
-    	   
-       }
-       else {
-    	   monsteps+=goldinrow-monsterinrow;
-       }
-       if(monsterincolumn>goldincolumn) {
-    	   monsteps+=monsterincolumn-goldincolumn;
-       }
-       else {
-    	   monsteps+=goldincolumn-monsterincolumn;
-       }
-       
-      if(steps<=monsteps) {
-    	  System.out.println("Minimum number of steps :"+steps);
+       int stepsofadventure=stepcalculate(adventure,gold);
+       int stepsofmonster=stepcalculate(monster,gold);
+       int stepsadventuretotrigger=stepcalculate(adventure,trigger);
+       int stepstriggertomonster=stepcalculate(trigger,monster);
+       int stepsmonstertogold=stepcalculate(monster,gold);
+     
+      if(stepsofadventure<=stepsofmonster) {
+    	  System.out.println("Minimum number of steps :"+stepsofadventure); 
+    	  System.out.println(footprint(stepsofadventure,adventure,gold,monster));
+      }
+      else if(stepsadventuretotrigger<=stepstriggertomonster){
+            System.out.println(stepsadventuretotrigger+stepstriggertomonster+stepsmonstertogold);
       }
       else {
-    	  System.out.println("No possible solution");
+    	  System.out.println("No possible solution");  
       }
-      System.out.println(strb.toString());
+      
+ 
 	}
 
 }
